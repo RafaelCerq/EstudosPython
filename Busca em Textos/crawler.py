32efaac5e1fa840b8101ec49e32aa6ccab164091
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-10-07 10:23:14 -0300

    Incluindo profundidade nas buscas

--- Busca em Textos/crawler.py
+++ Busca em Textos/crawler.py
@@ -3,42 +3,52 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 # Criando metodo crawl
-def crawl(pagina):
+def crawl(paginas, profundidade):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning())
-    http = urllib3.PoolManager()
-     
-    try:
-        dados_pagina = http.request('GET', pagina)
-    except:
-        print('Erro ao Abrir a pagina ' + pagina)
+    for i in range(profundidade):
+        novas_paginas = set();
         
-    
-    sopa = BeautifulSoup(dados_pagina.data, "lxml")
-    links = sopa.find_all('a')
-    contador = 1
-    for link in links:
-        #print(str(link.contents) + " - " + str(link.get('href')))
-        #print(link.attrs)
-        #print('\n')
+        for pagina in paginas:
         
-        if ('href' in link.attrs):
-            url = urljoin(pagina, str(link.get('href')))
-            #if url != link.get('href'):
-                #print(url)
-                #print(link.get('href'))
-                
-            if url.find("'") != -1:
+            http = urllib3.PoolManager()
+             
+            try:
+                dados_pagina = http.request('GET', pagina)
+            except:
+                print('Erro ao Abrir a pagina ' + pagina)
                 continue
+                
             
-            #print(url)
-            url = url.split('#')[0]
-            #print(url)
-            #print('\n')
-
-            contador = contador + 1
-    
-    print(contador)
+            sopa = BeautifulSoup(dados_pagina.data, "lxml")
+            links = sopa.find_all('a')
+            contador = 1
+            for link in links:
+                #print(str(link.contents) + " - " + str(link.get('href')))
+                #print(link.attrs)
+                #print('\n')
+                
+                if ('href' in link.attrs):
+                    url = urljoin(pagina, str(link.get('href')))
+                    #if url != link.get('href'):
+                        #print(url)
+                        #print(link.get('href'))
+                        
+                    if url.find("'") != -1:
+                        continue
+                    
+                    #print(url)
+                    url = url.split('#')[0]
+                    #print(url)
+                    #print('\n')
+                    
+                    if url[0:4] == 'http':
+                        novas_paginas.add(url);
+        
+                    contador = contador + 1
+            
+            paginas = novas_paginas
+            print(contador)
         
         
-
-crawl('https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o')
+listapaginas = ['https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o']
+crawl(listapaginas, 2)
