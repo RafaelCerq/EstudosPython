b752e4a6a43d0a003a31dac39cf2738717cb983e
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-10-10 15:55:19 -0300

    script page rank

--- Busca em Textos/bd.sql
+++ Busca em Textos/bd.sql
@@ -3,7 +3,7 @@ use indice;
 
 create table urls (
 	idurl int not null auto_increment,
-	url varchar(1000) not null,
+	url varchar(2000) not null,
 	constraint pk_urls_idurl primary key (idurl)
 );
 
@@ -29,3 +29,32 @@ create table palavra_localizacao (
 
 create index idx_palavra_localizacao_idpalavra on palavra_localizacao (idpalavra);
 
+
+
+create table url_ligacao (
+	idurl_ligacao int not null auto_increment,
+    idurl_origem int not null,
+    idurl_destino int not null,
+    constraint pk_idurl_ligacao primary key (idurl_ligacao),
+    constraint fk_url_ligacao_idurl_origem foreign key (idurl_origem) references urls (idurl),
+    constraint fk_url_ligacao_idurl_destino foreign key (idurl_destino) references urls (idurl)
+);
+create index idx_url_ligacao_idurl_origem on url_ligacao (idurl_origem);
+create index idx_url_ligacao_idurl_destino on url_ligacao (idurl_destino);
+
+create table url_palavra (
+	idurl_palavra int not null auto_increment,
+    idpalavra int not null,
+    idurl_ligacao int not null,
+    constraint pk_url_palavra_idurl_palavra primary key (idurl_palavra),
+    constraint fk_url_palavra_idpalavra foreign key (idpalavra) references palavras (idpalavra),
+    constraint fk_url_palavra_idurl_ligacao foreign key (idurl_ligacao) references url_ligacao (idurl_ligacao)
+);
+create index idx_url_palavra_idpalavra on url_palavra (idpalavra);
+
+create table page_rank (
+	idurl int not null,
+    nota float not null,
+    constraint pk_page_rank_idurl primary key (idurl),
+    constraint fk_page_rank_idurl foreign key (idurl) references urls (idurl)
+);
